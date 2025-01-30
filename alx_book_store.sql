4155159bad1926097fd8fd09623b73498ad2addf
olami851 <abdhamid851@gmail.com>
2025-01-30 08:09:03 +0100

    A Magical Database for Your Dream Online Bookstore Adventure!

---
+++ alx_book_store.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE alx_book_store;
+
+USE alx_book_store;
+
+SHOW DATABASES;
+
+SELECT DATABASE();
+
+-- Books Table: Stores information about books available in the bookstore.
+
+CREATE TABLE Books(
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR (130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+-- Authors Table: Stores information about authors.
+CREATE TABLE Authors(
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+SELECT* FROM Books;
+
+SELECT* FROM Authors;
+
+
+-- Customers Table: Stores information about customers.
+CREATE TABLE Customers(
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+-- Orders Table: Stores information about orders placed by customers.
+CREATE TABLE Orders(
+    order_id INT PRIMARY KEY,
+    customer_id INT, 
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- Order_Details Table: Stores information about the books included in each order.
+CREATE TABLE Order_Details(
+    order_detail_id INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
